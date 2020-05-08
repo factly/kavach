f1c10ab6fcee2c7aab4284c5f12d3db779e2a7c6
Monark <monark@factly.in>
2020-05-08 14:31:54 +0530

    new login ui added

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,59 +1,67 @@
-import React from "react";
-import "./login.css";
-import { Link } from "react-router-dom";
-import logo from "../../assets/logo.svg";
-import { Input, Form, Button, Typography } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import React from 'react';
+import './login.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { Input, Form, Button, Card, Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 function Login() {
   const [config, setConfig] = React.useState({});
-
-  const { Title } = Typography;
 
   React.useEffect(() => {
     var obj = {};
 
     window.location.search
-      .split("?")
-      .filter((each) => each.trim() !== "")
+      .split('?')
+      .filter((each) => each.trim() !== '')
       .forEach((each) => {
-        var temp = each.split("=");
+        var temp = each.split('=');
         obj[temp[0]] = temp[1];
       });
 
-    if (!obj["request"]) {
+    if (!obj['request']) {
       window.location.href =
-        process.env.REACT_APP_KRATOS_PUBLIC_URL +
-        "/self-service/browser/flows/login";
+        process.env.REACT_APP_KRATOS_PUBLIC_URL + '/self-service/browser/flows/login';
     }
 
     fetch(
       process.env.REACT_APP_KRATOS_PUBLIC_URL +
-        "/self-service/browser/flows/requests/login?request=" +
-        obj["request"]
+        '/self-service/browser/flows/requests/login?request=' +
+        obj['request'],
     )
-      .then((res) => res.json())
-      .then((res) => setConfig(res.methods.password.config));
+      .then((res) => {
+        if (res.status === 200){
+          return res.json()
+        } else {
+          throw new Error(res.status)
+        }
+      })
+      .then((res) => setConfig(res.methods.password.config))
+      .catch((err) => {
+        console.log(err)
+        window.location.href =
+          process.env.REACT_APP_KRATOS_PUBLIC_URL + '/self-service/browser/flows/login';
+      });
   }, []);
 
   if (!config.action) return null;
 
   const onFinish = (values) => {
-    var loginForm = document.createElement("form");
+    var loginForm = document.createElement('form');
     loginForm.action = config.action;
     loginForm.method = config.method;
-    loginForm.style.display = "none";
+    loginForm.style.display = 'none';
 
-    var identifierInput = document.createElement("input");
-    identifierInput.name = "identifier";
+    var identifierInput = document.createElement('input');
+    identifierInput.name = 'identifier';
     identifierInput.value = values.email;
 
-    var passwordInput = document.createElement("input");
-    passwordInput.name = "password";
+    var passwordInput = document.createElement('input');
+    passwordInput.name = 'password';
     passwordInput.value = values.password;
 
-    var csrfInput = document.createElement("input");
-    csrfInput.name = "csrf_token";
+    var csrfInput = document.createElement('input');
+    csrfInput.name = 'csrf_token';
     csrfInput.value = config.fields[2].value;
 
     loginForm.appendChild(identifierInput);
@@ -67,56 +75,46 @@ function Login() {
 
   return (
     <div className="login">
-      <div className="top">
-        <div className="header">
+      <Row className="header">
+        <Col span={6}>
           <img alt="logo" className="logo" src={logo} />
-          <span className="title">Go Commerce</span>
-        </div>
-        <div className="desc">From A to Z datasets</div>
-      </div>
-      <div className="login-form">
-        <Title level={3} className="login-form-title">
-          Admin Login
-        </Title>
-        <Form name="admin_login" onFinish={onFinish}>
+        </Col>
+        <Col span={18}>
+          <span className="title">Identity</span>
+        </Col>
+      </Row>
+      <Card
+        actions={[<Link to={'/auth/registration'}>Register now!</Link>]}
+        title="Login"
+        style={{ width: 400 }}
+      >
+        <Form name="login" onFinish={onFinish}>
           <Form.Item
             name="email"
             rules={[
-              { required: true, message: "Please input your Email!" },
-              { type: "email", message: "Please input valid Email!" },
+              { required: true, message: 'Please input your Email!' },
+              { type: 'email', message: 'Please input valid Email!' },
             ]}
           >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
+            rules={[{ required: true, message: 'Please input your Password!' }]}
           >
-            <Input
+            <Input.Password
               prefix={<LockOutlined className="site-form-item-icon" />}
               type="password"
               placeholder="Password"
             />
           </Form.Item>
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
+            <Button form="login" type="primary" htmlType="submit" block>
               Log in
             </Button>
           </Form.Item>
         </Form>
-      </div>
-      <div>
-        <Title level={4}>
-          <Link to="/auth/registration">Sign up</Link>
-        </Title>
-      </div>
+      </Card>
     </div>
   );
 }
--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -1,61 +1,68 @@
-import React from "react";
-import "./registration.css";
-import logo from "../../assets/logo.svg";
-import { Input, Form, Button, Typography } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import React from 'react';
+import './registration.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { Input, Form, Button, Card, Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 function Registration() {
   const [config, setConfig] = React.useState({});
-
-  const { Title } = Typography;
 
   React.useEffect(() => {
     var obj = {};
 
     window.location.search
-      .split("?")
-      .filter((each) => each.trim() !== "")
+      .split('?')
+      .filter((each) => each.trim() !== '')
       .forEach((each) => {
-        var temp = each.split("=");
+        var temp = each.split('=');
         obj[temp[0]] = temp[1];
       });
 
-    if (!obj["request"]) {
+    if (!obj['request']) {
       window.location.href =
-        process.env.REACT_APP_KRATOS_PUBLIC_URL +
-        "/self-service/browser/flows/registration";
+        process.env.REACT_APP_KRATOS_PUBLIC_URL + '/self-service/browser/flows/registration';
     }
 
     fetch(
       process.env.REACT_APP_KRATOS_PUBLIC_URL +
-        "/self-service/browser/flows/requests/registration?request=" +
-        obj["request"]
+        '/self-service/browser/flows/requests/registration?request=' +
+        obj['request'],
     )
-      .then((res) => res.json())
-      .then((res) => setConfig(res.methods.password.config));
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          throw new Error(res.status);
+        }
+      })
+      .then((res) => setConfig(res.methods.password.config))
+      .catch((err) => {
+        console.log(err);
+        window.location.href =
+          process.env.REACT_APP_KRATOS_PUBLIC_URL + '/self-service/browser/flows/registration';
+      });
   }, []);
 
   if (!config.action) return null;
 
   const onFinish = (values) => {
-    var loginForm = document.createElement("form");
+    var loginForm = document.createElement('form');
     loginForm.action = config.action;
     loginForm.method = config.method;
-    loginForm.style.display = "none";
+    loginForm.style.display = 'none';
 
-    var identifierInput = document.createElement("input");
-    identifierInput.name = "traits.email";
+    var identifierInput = document.createElement('input');
+    identifierInput.name = 'traits.email';
     identifierInput.value = values.email;
 
-    var passwordInput = document.createElement("input");
-    passwordInput.name = "password";
+    var passwordInput = document.createElement('input');
+    passwordInput.name = 'password';
     passwordInput.value = values.password;
 
-    var csrfInput = document.createElement("input");
-    csrfInput.name = "csrf_token";
-    csrfInput.value = config.fields.find(
-      (value) => value.name === "csrf_token"
-    ).value;
+    var csrfInput = document.createElement('input');
+    csrfInput.name = 'csrf_token';
+    csrfInput.value = config.fields.find((value) => value.name === 'csrf_token').value;
 
     loginForm.appendChild(identifierInput);
     loginForm.appendChild(passwordInput);
@@ -68,51 +75,45 @@ function Registration() {
 
   return (
     <div className="registration">
-      <div className="top">
-        <div className="header">
+      <Row className="header">
+        <Col span={6}>
           <img alt="logo" className="logo" src={logo} />
-          <span className="title">Go Commerce</span>
-        </div>
-        <div className="desc">From A to Z datasets</div>
-      </div>
-      <div className="registration-form">
-        <Title level={3} className="registration-form-title">
-          Admin Registration
-        </Title>
-        <Form name="admin_registration" onFinish={onFinish}>
+        </Col>
+        <Col span={18}>
+          <span className="title">Identity</span>
+        </Col>
+      </Row>
+      <Card
+        actions={[<Link to={'/auth/login'}>Log In!</Link>]}
+        title="Registration"
+        style={{ width: 400 }}
+      >
+        <Form name="registration" onFinish={onFinish}>
           <Form.Item
             name="email"
             rules={[
-              { required: true, message: "Please input your Email!" },
-              { type: "email", message: "Please input valid Email!" },
+              { required: true, message: 'Please input your Email!' },
+              { type: 'email', message: 'Please input valid Email!' },
             ]}
           >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
+            rules={[{ required: true, message: 'Please input your Password!' }]}
           >
-            <Input
+            <Input.Password
               prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
               placeholder="Password"
             />
           </Form.Item>
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="registration-form-button"
-            >
+            <Button form="registration" type="primary" htmlType="submit" block>
               Log in
             </Button>
           </Form.Item>
         </Form>
-      </div>
+      </Card>
     </div>
   );
 }
