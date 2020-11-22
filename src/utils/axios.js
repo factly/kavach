2c92a3facb300c8774f50dc62e3dbcfdb80f7b22
Shashi Deshetti <deshetti@gmail.com>
2020-11-22 16:54:36 -0600

    fix(axios_middleware): setting passing cookie in the axios middleware

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 function createAxiosAuthMiddleware() {
   return () => (next) => (action) => {
     axios.defaults.baseURL = window.REACT_APP_API_URL;
+    axios.defaults.withCredentials = true;
     return next(action);
   };
 }
