ece1a46dbeb71f40ce9a8510b5c5af571a332b1f
Shashi Deshetti <deshetti@gmail.com>
2020-11-22 16:50:02 -0600

    removed setting CORS headers in the code

--- action/routes.go
+++ action/routes.go
@@ -26,17 +26,6 @@ func RegisterRoutes() http.Handler {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
 
-	// r.Use(cors.Handler(cors.Options{
-	// 	// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-	// 	AllowedOrigins: []string{"https://kavach.factly.org"},
-	// 	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	// 	ExposedHeaders:   []string{"Link"},
-	// 	AllowCredentials: false,
-	// 	MaxAge:           300, // Maximum value not ignored by any of major browsers
-	// }))
-
 	if viper.IsSet("mode") && viper.GetString("mode") == "development" {
 		r.Get("/swagger/*", httpSwagger.WrapHandler)
 		log.Printf("Swagger @ http://localhost:5000/swagger/index.html")
