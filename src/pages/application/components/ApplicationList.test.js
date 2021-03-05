730861804d54bc264af75f4ceb4a90f72a1dbe9f
Bhumi101097 <bhumi.patel@factly.in>
2021-03-05 13:30:58 +0530

    Unit test cases for application

---
+++ src/pages/application/components/ApplicationList.test.js
@@ -0,0 +1,195 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { useDispatch, Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { Popconfirm, Button, Table } from 'antd';
+
+import '../../../matchMedia.mock';
+import ApplicationList from './ApplicationList';
+import { getApplications, deleteApplication } from '../../../actions/application';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+jest.mock('../../../actions/application', () => ({
+  getApplications: jest.fn(),
+  deleteApplication: jest.fn(),
+}));
+
+let mockedDispatch, store;
+
+let state = {
+  application: {
+    req: [
+      {
+        data: [1, 2],
+        query: {
+          page: 1,
+          limit: 5,
+        },
+        total: 2,
+      },
+    ],
+    details: {
+      '1': {
+        id: 1,
+        created_at: '2020-09-09T06:49:36.566567Z',
+        updated_at: '2020-09-09T06:49:36.566567Z',
+        name: 'Application1',
+        description: 'description',
+        url : 'url1',
+      },
+      '2': {
+        id: 2,
+        created_at: '2020-09-09T06:49:54.027402Z',
+        updated_at: '2020-09-09T06:49:54.027402Z',
+        name: 'Application2',
+        description: 'description',
+        url : 'url2',
+      },
+    },
+    loading: false,
+  },
+};
+
+describe('Application List component', () => {
+  describe('snapshot testing', () => {
+    beforeEach(() => {
+      store = mockStore({});
+      store.dispatch = jest.fn();
+      mockedDispatch = jest.fn();
+      useDispatch.mockReturnValue(mockedDispatch);
+    });
+    it('should render the component', () => {
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <Router>
+            <ApplicationList />
+          </Router>
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+    });
+    it('should match component when loading', () => {
+      state.application.loading = true;
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <Router>
+            <ApplicationList />
+          </Router>
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+    });
+    it('should match component with categories', () => {
+      state.application.loading = false;
+      store = mockStore(state);
+      const tree = mount(
+        <Provider store={store}>
+          <Router>
+            <ApplicationList />
+          </Router>
+        </Provider>,
+      );
+      expect(tree).toMatchSnapshot();
+      expect(mockedDispatch).toHaveBeenCalledTimes(1);
+
+      expect(getApplications).toHaveBeenCalledWith();
+    });
+  });
+  describe('component testing', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      mockedDispatch = jest.fn(() => new Promise((resolve) => resolve(true)));
+      useDispatch.mockReturnValue(mockedDispatch);
+    });
+    it('should change the page', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <Router>
+              <ApplicationList />
+            </Router>
+          </Provider>,
+        );
+      });
+      const table = wrapper.find(Table);
+      table.props().pagination.onChange(3);
+      wrapper.update();
+      const updatedTable = wrapper.find(Table);
+      expect(updatedTable.props().pagination.current).toEqual(3);
+    });
+    it('should delete application', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <Router>
+              <ApplicationList />
+            </Router>
+          </Provider>,
+        );
+      });
+      const button = wrapper.find(Button).at(1);
+      expect(button.text()).toEqual('Delete');
+
+      button.simulate('click');
+      const popconfirm = wrapper.find(Popconfirm);
+      popconfirm
+        .findWhere((item) => item.type() === 'button' && item.text() === 'OK')
+        .simulate('click');
+      expect(deleteApplication).toHaveBeenCalled();
+      expect(deleteApplication).toHaveBeenCalledWith(1);
+      expect(getApplications).toHaveBeenCalledWith();
+    });
+    it('should edit application', () => {
+      store = mockStore(state);
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <Router>
+              <ApplicationList />
+            </Router>
+          </Provider>,
+        );
+      });
+      const link = wrapper.find(Link).at(0);
+      const button = link.find(Button).at(0);
+      expect(button.text()).toEqual('Edit');
+      expect(link.prop('to')).toEqual('/applications/1/edit');
+    });
+    it('should have no delete and edit buttons', () => {
+      store = mockStore({
+        application: {
+          req: [],
+        },
+      });
+      let wrapper;
+      act(() => {
+        wrapper = mount(
+          <Provider store={store}>
+            <Router>
+              <ApplicationList />
+            </Router>
+          </Provider>,
+        );
+      });
+
+      const button = wrapper.find(Button);
+      expect(button.length).toEqual(0);
+    });
+  });
+}); 
