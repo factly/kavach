29e0d18b8ebe97e3db5b8340b102c8889171cec5
Bhumi101097 <bhumi.patel@factly.in>
2021-03-05 13:23:58 +0530

    Wrote Unit test cases for organisation

--- src/pages/organisation/index.test.js
+++ src/pages/organisation/index.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 import { act } from '@testing-library/react';
 import { shallow, mount } from 'enzyme';
 
 import '../../matchMedia.mock';
 import OrganisationCreate from './index';
 import { addOrganisation } from '../../actions/organisations';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 jest.mock('../../actions/organisations', () => ({
   addOrganisation: jest.fn(),
@@ -21,9 +26,12 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('Organisations index component', () => {
+  let store;
   const mockedDispatch = jest.fn();
   mockedDispatch.mockReturnValue(Promise.resolve());
   useDispatch.mockReturnValue(mockedDispatch);
+  store = mockStore({});
+  store.dispatch = jest.fn(() => ({}));
 
   const push = jest.fn();
   useHistory.mockReturnValue({ push });
@@ -31,16 +39,38 @@ describe('Organisations index component', () => {
     it('should render the component', () => {
       let component;
       act(() => {
-        component = shallow(<OrganisationCreate />);
+        component = mount(
+          <Provider store={store}>
+            <OrganisationCreate />
+          </Provider>  
+        );
       });
       expect(component).toMatchSnapshot();
     });
   });
   describe('component testing', () => {
+    store = mockStore({
+      organisations: {
+        ids: [1],
+        details: { 1: { id: 1, name: 'organisation' } },
+        loading: false,
+        selected: 1,
+      },
+      media: {
+        req : [],
+        details: {},
+        loading: false,
+      },
+    })
+
     let wrapper;
     beforeEach(() => {
       act(() => {
-        wrapper = mount(<OrganisationCreate />);
+        wrapper = mount(
+          <Provider store={store}>
+            <OrganisationCreate />
+          </Provider>
+        );
       });
     });
     afterEach(() => {
@@ -55,7 +85,7 @@ describe('Organisations index component', () => {
           .simulate('change', { target: { value: 'title' } });
         wrapper
           .find('FormItem')
-          .at(1)
+          .at(2)
           .find('TextArea')
           .at(0)
           .simulate('change', { target: { value: 'description' } });
@@ -69,7 +99,9 @@ describe('Organisations index component', () => {
         expect(addOrganisation).toHaveBeenCalledTimes(1);
         expect(addOrganisation).toHaveBeenCalledWith({
           title: 'title',
+          slug: 'title',
           description: 'description',
+          featured_medium_id: undefined,
         });
         expect(push).toHaveBeenCalledWith('/settings');
         done();
