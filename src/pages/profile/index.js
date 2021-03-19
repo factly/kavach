43869839beb441bd636d6db42b1815e2de246d1c
Bhumi101097 <bhumi.patel@factly.in>
2021-03-19 11:48:16 +0530

    Display profile Pic if available

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 import MediaSelector from '../../components/MediaSelector';
 import { maker, checker } from '../../utils/sluger';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUserProfile, updateProfile } from '../../actions/profile';
+import { updateProfile } from '../../actions/profile';
 
 const layout = {
   labelCol: {
@@ -31,10 +31,6 @@ function Profile() {
       loading: state.profile.loading,
     };
   });
-
-  React.useEffect(() => {
-    dispatch(getUserProfile());
-  }, [dispatch]);
 
   const update = (values) => {
     values.birth_date = values.birth_date
