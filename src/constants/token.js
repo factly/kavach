aa895748d80285ba1aa4257e8fd2be3202b98436
shreeharsha-factly <harsha@factly.in>
2021-03-21 19:34:48 +0530

    add actions & constants for application tokens

---
+++ src/constants/token.js
@@ -0,0 +1,9 @@
+//Actions
+export const ADD_TOKEN = 'ADD_TOKEN';
+export const ADD_TOKENS = 'ADD_TOKENS';
+export const ADD_TOKENS_REQUEST = 'ADD_TOKENS_REQUEST';
+export const SET_TOKENS_LOADING = 'SET_TOKENS_LOADING';
+export const RESET_TOKENS = 'RESET_TOKENS';
+
+//API
+export const TOKENS_API = '/tokens';
