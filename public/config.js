c3d781cc22ea4956600a2ca330fb439fd48dfea0
hetjagani <hetpatel572@gmail.com>
2021-06-11 15:08:08 -0700

    Moved kavach-web files in web folder

--- public/config.js
+++
@@ -1,7 +0,0 @@
-window.REACT_APP_KRATOS_PUBLIC_URL = 'http://127.0.0.1:4455/.ory/kratos/public';
-window.REACT_APP_API_URL = 'http://127.0.0.1:4455/.factly/kavach/server';
-window.PUBLIC_URL = 'http://127.0.0.1:4455/.factly/kavach/web';
-window.REACT_APP_DEGA_PUBLIC_URL = 'http://127.0.0.1:4455/.factly/dega/studio';
-window.REACT_APP_BINDU_PUBLIC_URL = 'http://127.0.0.1:4455/.factly/bindu/studio';
-window.REACT_APP_COMPANION_URL = 'http://127.0.0.1:3020';
-
