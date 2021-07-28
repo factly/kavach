d7a9d8f1a776ad976a4044233438295ea246eeb3
Bhumi101097 <bhumi.patel@factly.in>
2021-07-28 12:50:32 +0530

    Breadcrumb changes

--- web/src/components/PageHeader/index.js
+++ web/src/components/PageHeader/index.js
@@ -3,10 +3,14 @@ import { Link, useLocation } from 'react-router-dom';
 import { PageHeader as AntPageHeader } from 'antd';
 import routes from '../../config/routes';
 import _ from 'lodash';
+import { useSelector } from 'react-redux';
+import { matchPath } from 'react-router';
 
 function PageHeader() {
+  const state = useSelector((state) => state);
   const location = useLocation();
   const pathSnippets = location.pathname.split('/').filter((i) => i);
+  const entity = pathSnippets[0];
 
   const breadcrumbItems = useMemo(() => {
     const urlBreadcrumbItems = pathSnippets.map((empty, index) => {
@@ -19,7 +23,28 @@ function PageHeader() {
           breadcrumbName: route.title,
         };
       } else {
-        return null;
+        if (pathSnippets.includes('edit') && !state[entity].loading) {
+          const generatedReferenceURL = `/${pathSnippets.slice(0, index - 1).join('/')}`
+            .concat('/:id/')
+            .concat(pathSnippets.slice(index, index + 2).join('/'));
+          let match = matchPath(location.pathname, {
+            path: generatedReferenceURL,
+            exact: true,
+            strict: false,
+          });
+          if (match) {
+            const route = _.find(routes, { path: generatedReferenceURL });
+            if (route) {
+              const entityId = pathSnippets[index - 1];
+              return {
+                path: route.path,
+                breadcrumbName: state[entity].details[entityId].name,
+              };
+            }
+          }
+        } else {
+          return null;
+        }
       }
     });
     return [
@@ -47,13 +72,17 @@ function PageHeader() {
     return <Link to={route.path}>{route.breadcrumbName}</Link>;
   };
 
-  return (
-    <AntPageHeader
-      ghost={false}
-      title={lastItem.title}
-      breadcrumb={{ itemRender, routes: breadcrumbItems }}
-    />
-  );
+  if (state[entity] && !state[entity].loading)
+    return (
+      <AntPageHeader
+        ghost={false}
+        title={lastItem.title}
+        breadcrumb={{ itemRender, routes: breadcrumbItems }}
+      />
+    );
+  else {
+    return null;
+  }
 }
 
 export default PageHeader;
