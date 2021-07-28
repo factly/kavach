c694f1966a51f13bcba058384899552459482a7a
Bhumi101097 <bhumi.patel@factly.in>
2021-07-28 17:43:00 +0530

    Fetch organistaions

--- web/src/pages/application/EditApplication.js
+++ web/src/pages/application/EditApplication.js
@@ -3,6 +3,7 @@ import ApplicationEditForm from './components/ApplicationForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { Skeleton } from 'antd';
 import { updateApplication, getApplication } from '../../actions/application';
+import { getOrganisations } from '../../actions/organisations';
 import { useHistory } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
@@ -24,9 +25,10 @@ function EditApplication() {
   if (loading && !application) return <Skeleton />;
 
   const onUpdate = (values) => {
-    dispatch(updateApplication({ ...application, ...values })).then(() =>
-      history.push('/applications'),
-    );
+    dispatch(updateApplication({ ...application, ...values })).then(() => {
+      dispatch(getOrganisations());
+      history.push('/applications');
+    });
   };
   return <ApplicationEditForm data={application} onCreate={onUpdate} />;
 }
