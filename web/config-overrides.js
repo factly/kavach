fc3cf846484dd55cb1ec4f8a4728103fa774ad5f
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-10-20 17:19:31 +0530

    add config override in andt

---
+++ web/config-overrides.js
@@ -0,0 +1,14 @@
+const AntDesignThemePlugin = require('antd-theme-webpack-plugin');
+const path = require('path');
+const options = {
+  antDir: path.join(__dirname, './node_modules/antd'),
+  stylesDir: path.join(__dirname, './src/styles'),
+  varFile: path.join(__dirname, './src/styles/variables.less'),
+  themeVariables: ['@primary-color', '@link-color'],
+  publicPath: process.env.NODE_ENV === 'production' ? 'kavach.factly.org' : '/.factly/kavach/web/',
+  indexFileName: 'index.html',
+};
+module.exports = function override(config, env) {
+  config.plugins.push(new AntDesignThemePlugin(options));
+  return config;
+};
