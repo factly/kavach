6a880e7854766f67896f510052d08750e4dd9857
vsumit89 <sumit.vishwakarma@factly.in>
2021-10-25 11:49:52 +0530

    style(web): new branch to resolve conflicting issues

--- web/src/pages/application/components/ApplicationList.js
+++ web/src/pages/application/components/ApplicationList.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Popconfirm, Button, Table, Space, Avatar, Tooltip } from 'antd';
-
+import {
+  Popconfirm,
+  Button,
+  Table,
+  Skeleton,
+  Space,
+  Avatar,
+  Tooltip,
+  Card,
+  Row,
+  Col,
+  Icon,
+} from 'antd';
+import { PlayCircleFilled, EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApplications, deleteApplication } from '../../../actions/application';
 import { getOrganisations } from '../../../actions/organisations';
 import { Link } from 'react-router-dom';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 function ApplicationList() {
   const dispatch = useDispatch();
@@ -13,7 +24,6 @@ function ApplicationList() {
     page: 1,
     limit: 5,
   });
-
   const { applications, loading, total } = useSelector((state) => {
     const node = state.applications.req[0];
 
@@ -34,120 +44,82 @@ function ApplicationList() {
   const fetchApplications = () => {
     dispatch(getApplications());
   };
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      width: '15%',
-    },
-    {
-      title: 'Slug',
-      dataIndex: 'slug',
-      key: 'slug',
-      width: '15%',
-    },
-    {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-      width: '40%',
-      ellipsis: true,
-    },
-    {
-      title: 'Users',
-      dataIndex: 'users',
-      key: 'users',
-      width: '30%',
-      render: (_, record) => {
-        const maxAvatar = 4;
-        return (
-          <Avatar.Group>
-            {record.users &&
-              record.users.length > 0 &&
-              record.users.slice(0, maxAvatar).map((each) => (
-                <Tooltip title={each.email} placement="top">
-                  {each.medium && each.medium.url ? (
-                    <Avatar src={each.medium.url.proxy}></Avatar>
-                  ) : (
-                    <Avatar
-                      style={{
-                        backgroundColor:
-                          '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0'),
-                      }}
-                    >
-                      {each.email.charAt(0).toUpperCase()}
-                    </Avatar>
-                  )}
-                </Tooltip>
-              ))}
-            {record.users.length > 4 ? (
-              <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
-                +{record.users.length - maxAvatar}
-              </Avatar>
-            ) : (
-              <></>
-            )}
-          </Avatar.Group>
-        );
-      },
-    },
-    {
-      title: 'Action',
-      dataIndex: 'operation',
-      width: '20%',
-      render: (_, record) => {
-        return (
-          <span>
-            <Link
-              className="ant-dropdown-link"
-              style={{
-                marginRight: 8,
-              }}
-              to={`/applications/${record.id}/edit`}
-            >
-              <Button>
-                <EditOutlined />
-              </Button>
-            </Link>
-            <Popconfirm
-              title="Sure to Delete?"
-              onConfirm={() =>
-                dispatch(deleteApplication(record.id)).then(() => {
-                  dispatch(getOrganisations());
-                  fetchApplications();
-                })
+  let applicationList = [];
+  const ApplicationCard = (props) => {
+    return (
+      <Card
+        hoverable
+        loading={loading}
+        style={{ width: 280, marginTop: 10 }}
+        cover={
+          loading ? (
+            <Card loading={true}></Card>
+          ) : (
+            <img
+              alt="example"
+              src={
+                props.application.medium && props.application.medium_id
+                  ? props.application.medium.url.proxy
+                  : 'https://cdn5.vectorstock.com/i/thumb-large/99/49/bold-mid-century-abstract-drawing-vector-28919949.jpg'
               }
-            >
-              <Link to="" className="ant-dropdown-link">
-                <Button type="danger">
-                  <DeleteOutlined />
-                </Button>
-              </Link>
-            </Popconfirm>
-          </span>
-        );
-      },
-    },
-  ];
+            />
+          )
+        }
+        actions={[
+          <Link
+            to={loading ? '' : `/applications/${props.application.id}/edit`}
+            className="ant-dropdown-link"
+          >
+            <EditOutlined key="edit" style={{ fontSize: '150%' }} />
+          </Link>,
+          <Popconfirm
+            title="Sure to Delete?"
+            onConfirm={() =>
+              dispatch(deleteApplication(props.application.id)).then(() => {
+                dispatch(getOrganisations());
+                fetchApplications();
+              })
+            }
+          >
+            <Link to="" className="ant-dropdown-link">
+              <DeleteOutlined style={{ fontSize: '150%' }} />
+            </Link>
+          </Popconfirm>,
+          <a href={props.application.url}>
+            <PlayCircleFilled style={{ fontSize: '150%' }} />
+          </a>,
+        ]}
+      >
+        <Card.Meta
+          title={loading ? '' : props.application.name}
+          description={loading ? '' : props.application.description}
+          style={{ textAlign: 'center' }}
+        />
+      </Card>
+    );
+  };
 
+  const ApplicationRow = (props) => {
+    return (
+      <Row style={{ width: '125%', display: 'flex', justifyContent: 'flex-start', gap: '5%' }}>
+        {props.applications.map((application, index) => (
+          <ApplicationCard key={index} application={application}></ApplicationCard>
+        ))}
+      </Row>
+    );
+  };
+  if (loading) {
+    return <Skeleton />;
+  } else {
+    for (var i = 0; i < Math.ceil(applications.length / 3); i++) {
+      applicationList.push(applications.slice(3 * i, 3 * i + 3));
+    }
+  }
   return (
     <Space direction="vertical">
-      <Table
-        bordered
-        columns={columns}
-        dataSource={applications}
-        loading={loading}
-        rowKey={'id'}
-        pagination={{
-          total: total,
-          current: filters.page,
-          pageSize: filters.limit,
-          onChange: (pageNumber, pageSize) =>
-            setFilters({ ...filters, page: pageNumber, limit: pageSize }),
-        }}
-      />
+      {applicationList.map((applicationRow, index) => (
+        <ApplicationRow key={index} applications={applicationRow} />
+      ))}
     </Space>
   );
 }
