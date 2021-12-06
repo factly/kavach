aedf771409e273ab090d111650549e296e4d26ba
vsumit89 <sumit.vishwakarma@factly.in>
2021-12-06 13:20:34 +0530

    removing the changes made in this branch

--- web/src/pages/organisation/index.js
+++ web/src/pages/organisation/index.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
-import { Button, Form, Input, Modal } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { Button, Form, Input } from 'antd';
+import { useDispatch } from 'react-redux';
 import { addOrganisation } from './../../actions/organisations';
 import { useHistory } from 'react-router-dom';
 import { maker, checker } from '../../utils/sluger';
@@ -10,93 +10,56 @@ function OrganisationCreate() {
   const dispatch = useDispatch();
   const history = useHistory();
   const [form] = Form.useForm();
-  const [modalForm] = Form.useForm();
-  const {organisationCount, loading} = useSelector((state)=>{
-    const count = (state.organisations && state.organisations.ids) ? state.organisations.ids.length : null
-    return {
-      organisationCount: count,
-      loading: state.organisations.loading,
-    }
-  })
-  const onTitleChange = (form, string) => {
+
+  const onTitleChange = (string) => {
     form.setFieldsValue({
       slug: maker(string),
     });
   };
 
-  const [isModalVisible, setModalVisible] = useState((organisationCount==0)?true:false)
-  const changeModalVisibility = (visible)=>{
-    setModalVisible(visible)
-  }
-
-  const handleFormSubmit = (values)=>{
-    dispatch(addOrganisation(values)).then(history.push('/settings'))
-  }
-  React.useEffect(()=>{
-    if(organisationCount==0){
-      setModalVisible(true)
-    }else{
-      setModalVisible(false)
-    }
-  }, [organisationCount])
-
-  const FormComponent = ({form, name})=>{
-    return (
-      <Form
-        form={form}
-        name={name}
-        layout="vertical"
-        onFinish={(values)=>handleFormSubmit(values)}
-        style={{
-          width: '400px',
-        }}
-      >
-        <Form.Item name="title" label="Title">
-          <Input placeholder="Title" onChange={(e) => onTitleChange(form ,e.target.value)} />
-        </Form.Item>
-        <Form.Item
-          name="slug"
-          label="Slug"
-          rules={[
-            {
-              required: true,
-              message: 'Please input the slug!',
-            },
-            {
-              pattern: checker,
-              message: 'Please enter valid slug!',
-            },
-          ]}
-        >
-          <Input placeholder="Slug"></Input>
-        </Form.Item>
-        <Form.Item name="description" label="Description">
-          <Input.TextArea placeholder="Description" />
-        </Form.Item>
-        <Form.Item label="Upload Image" name="featured_medium_id">
-          <MediaSelector />
-        </Form.Item>
-        <Form.Item>
-          <Button form={name} type="primary" htmlType="submit" block>
-            Save
-          </Button>
-        </Form.Item>
-      </Form>
-    )
-  }
   return (
-    <>
-      <Modal
-        visible={isModalVisible}
-        closable={false}
-        footer={null}
-        >
-        <h2>Create an Organisation</h2>
-        <FormComponent form={modalForm} name="modal"/>
-      </Modal>
-      <FormComponent form={form} name="non-modal"/>
-    </>
+    <Form
+      form={form}
+      name="organisation_create"
+      layout="vertical"
+      onFinish={(values) => dispatch(addOrganisation(values)).then(history.push('/settings'))}
+      style={{
+        width: '400px',
+      }}
+    >
+      <Form.Item name="title" label="Title">
+        <Input placeholder="Title" onChange={(e) => onTitleChange(e.target.value)} />
+      </Form.Item>
+      <Form.Item
+        name="slug"
+        label="Slug"
+        rules={[
+          {
+            required: true,
+            message: 'Please input the slug!',
+          },
+          {
+            pattern: checker,
+            message: 'Please enter valid slug!',
+          },
+        ]}
+      >
+        <Input placeholder="Slug"></Input>
+      </Form.Item>
+      <Form.Item name="description" label="Description">
+        <Input.TextArea placeholder="Description" />
+      </Form.Item>
+      <Form.Item label="Upload Image" name="featured_medium_id">
+        <MediaSelector />
+      </Form.Item>
+      <Form.Item>
+        <Button form="organisation_create" type="primary" htmlType="submit" block>
+          Save
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
 
 export default OrganisationCreate;
+
