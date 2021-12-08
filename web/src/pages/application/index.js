ca8bcac7e12db4aa81c77e41729461f20d9eaeb8
vsumit89 <sumit.vishwakarma@factly.in>
2021-12-08 14:40:10 +0530

    changed the way applications are loaded into the component

--- web/src/pages/application/index.js
+++ web/src/pages/application/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import ApplicationList from './components/ApplicationList';
-import { Button } from 'antd';
+import { Button, Skeleton} from 'antd';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { addDefaultApplications } from '../../actions/application';
-
+import { addDefaultApplications, getApplications } from '../../actions/application';
+import ErrorComponent from '../../components/ErrorsAndImage/ErrorComponent';
 function Application() {
   const dispatch = useDispatch();
   var buttonFlag = true;
-  const { application, userDetails, loadingUsers, userID, loadingID } = useSelector((state) => {
+  React.useEffect(() => {
+    fetchApplications();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
+  const fetchApplications = () => {
+    dispatch(getApplications());
+  };
+
+  const { applicationData, userDetails, loadingUsers, userID, loadingID } = useSelector((state) => {
     return {
-      application: state.applications.req[0],
+      applicationData: state.applications,
       userDetails: state.users.details,
       loadingUsers: state.users.loading,
       userID: state.profile.details.id,
       loadingID: state.profile.loading,
     };
   });
-
   if (!loadingUsers && !loadingID) {
     if (userDetails[userID].permission.role === 'member') {
       buttonFlag = false;
@@ -30,16 +38,32 @@ function Application() {
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'end' }}>
-        {application && application.data.length === 0 ? (
-          <Button onClick={addDefaultApps}>Add Factly Applications</Button>
-        ) : null}
-        {buttonFlag ? (
-          <Link key="1" to="/applications/create">
-            <Button type="primary">New Application</Button>
-          </Link>
-        ) : null}
+        {
+          (buttonFlag) ?
+            (applicationData.loading) ? 
+              (<Skeleton/>)
+              :(applicationData.req[0].data.length===0) ?
+                  (
+                  <div>
+                    <Button onClick={addDefaultApps}>Add Factly Applications</Button>
+                    <Link key="1" to="/applications/create">
+                      <Button type="primary">New Application</Button>
+                    </Link>
+                  </div>
+                  ):
+                  (
+                  <Link key="1" to="/applications/create">
+                    <Button type="primary">New Application</Button>
+                  </Link>
+                  )
+            :(null)
+        }
       </div>
-      <ApplicationList />
+      {
+        (applicationData.loading) ? 
+          <Skeleton/> :
+          (applicationData.req[0].data.length) ? <ApplicationList applicationList={applicationData} permission={buttonFlag}/> : <ErrorComponent title="You have 0 applications" link="/organisation" message="Go Home"/>
+      }
     </div>
   );
 }
