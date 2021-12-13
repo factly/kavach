bb3f852c51ff451910e6a53574256125108e7f43
vsumit89 <sumit.vishwakarma@factly.in>
2021-12-13 18:37:09 +0530

    added the catch method for verification

--- web/src/pages/verification/index.js
+++ web/src/pages/verification/index.js
@@ -22,6 +22,9 @@ function Verification() {
         if (res && res.state === 'passed_challenge') {
           window.location.href = window.PUBLIC_URL + '/auth/login';
         }
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
   return (
