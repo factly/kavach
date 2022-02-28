71e0cd58e1379eb7b315aa2c5c423ac0f3b39946
vsumit89 <sumit.vishwakarma@factly.in>
2022-02-28 16:56:09 +0530

    removed spaces from application edit page

--- web/src/pages/application/spaces/index.js
+++ web/src/pages/application/spaces/index.js
@@ -48,7 +48,7 @@ function Spaces() {
               justifyContent: 'space-between',
             }}
           >
-            <h3> Application</h3>
+            <h3> Choose application :</h3>
             <Select
               style={{ width: '150px', marginLeft: 10 }}
               placeholder="choose application"
