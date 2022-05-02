2fe19287be60bac0b771ae0ebaf8a3d7f161778a
shreeharsha-factly <harsha@factly.in>
2022-05-02 15:00:46 +0530

    remove duplicate organisationtoken duplicate model

--- server/model/organisation.go
+++ server/model/organisation.go
@@ -9,13 +9,13 @@ import (
 // Organisation model definition
 type Organisation struct {
 	Base
-	Title             string              `gorm:"column:title" json:"title"`
-	Slug              string              `gorm:"column:slug" json:"slug"`
-	Description       string              `gorm:"column:description" json:"description"`
-	FeaturedMediumID  *uint               `gorm:"column:featured_medium_id;default:NULL" json:"featured_medium_id"`
-	Medium            *Medium             `gorm:"foreignKey:featured_medium_id" json:"medium"`
-	Applications      []Application       `gorm:"foreignKey:organisation_id" json:"applications"`
-	OrganisationUsers []OrganisationUser  `gorm:"foreignKey:organisation_id" json:"organisation_users"`
+	Title             string             `gorm:"column:title" json:"title"`
+	Slug              string             `gorm:"column:slug" json:"slug"`
+	Description       string             `gorm:"column:description" json:"description"`
+	FeaturedMediumID  *uint              `gorm:"column:featured_medium_id;default:NULL" json:"featured_medium_id"`
+	Medium            *Medium            `gorm:"foreignKey:featured_medium_id" json:"medium"`
+	Applications      []Application      `gorm:"foreignKey:organisation_id" json:"applications"`
+	OrganisationUsers []OrganisationUser `gorm:"foreignKey:organisation_id" json:"organisation_users"`
 	Roles             []OrganisationRole `gorm:"foreignKey:organisation_id" json:"roles"`
 }
 
@@ -35,15 +35,6 @@ type OrganisationToken struct {
 	Name           string        `gorm:"column:name" json:"name"`
 	Description    string        `gorm:"column:description" json:"description"`
 	OrganisationID uint          `gorm:"column:organisation_id" json:"organisation_id"`
-	Organisation   *Organisation `gorm:"foreignKey:organisation_id" json:"organisation"`
-	Token          string        `gorm:"column:token" json:"token"`
-}
-
-type OrganisationToken struct {
-	Base
-	Name           string        `gorm:"column:name" json:"name"`
-	Description    string        `gorm:"column:description" json:"description"`
-	OrganisationID *uint         `gorm:"column:organisation_id" json:"organisation_id"`
 	Organisation   *Organisation `gorm:"foreignKey:organisation_id" json:"organisation"`
 	Token          string        `gorm:"column:token" json:"token"`
 }
