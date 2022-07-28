6c69cb52a4594d64092802676f0e24183482533c
vsumit89 <sumit.vishwakarma@factly.in>
2022-07-28 15:37:53 +0530

    made namespace organisations

--- server/action/organisation/roles/route.go
+++ server/action/organisation/roles/route.go
@@ -5,7 +5,7 @@ import (
 	"github.com/go-chi/chi"
 )
 
-const namespace string = "namespace"
+const namespace string = "organisations"
 // Organisation Role Router
 func Router() chi.Router {
 	r := chi.NewRouter()
