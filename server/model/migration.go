a1d6c9caf58f2d7823cee5f8305f430460d19847
vsumit89 <sumit.vishwakarma@factly.in>
2022-08-19 19:03:12 +0530

    added loggerx init in migrate

--- server/model/migration.go
+++ server/model/migration.go
@@ -1,7 +1,10 @@
 package model
 
+import "github.com/factly/x/loggerx"
+
 // Migration does database migrations
 func Migration() {
+	loggerx.Init()
 	_ = DB.AutoMigrate(
 		&Organisation{},
 		&User{},
