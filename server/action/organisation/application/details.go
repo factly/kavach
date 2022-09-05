079ea3b9bf0d432883910e9f1712de20f3e3f482
vsumit89 <sumit.vishwakarma@factly.in>
2022-09-05 13:02:45 +0530

    fixed the users list in app details endpoint by adding a keto validation

--- server/action/organisation/application/details.go
+++ server/action/organisation/application/details.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/factly/kavach-server/model"
+	keto "github.com/factly/kavach-server/util/keto/relationTuple"
 	"github.com/factly/kavach-server/util/user"
 	"github.com/factly/x/errorx"
 	"github.com/factly/x/loggerx"
@@ -72,12 +73,41 @@ func details(w http.ResponseWriter, r *http.Request) {
 		Base: model.Base{
 			ID: uint(appID),
 		},
-	}).Preload("Users").Preload("Medium").Find(&app).Error
+	}).Preload("Medium").Find(&app).Error
 
 	if err != nil {
 		loggerx.Error(err)
 		errorx.Render(w, errorx.Parser(errorx.DBError()))
 		return
+	}
+
+	userIDs, err := keto.ListSubjectsByObjectID(namespace, "", fmt.Sprintf("org:%d:app:%d", orgID, appID))
+	if err != nil {
+		loggerx.Error(err)
+		errorx.Render(w, errorx.Parser(errorx.RecordNotFound()))
+		return
+	}
+
+	for _, userID := range userIDs {
+		uID, err := strconv.Atoi(userID)
+		if err != nil {
+			loggerx.Error(err)
+			errorx.Render(w, errorx.Parser(errorx.DecodeError()))
+			return
+		}
+
+		var userModel model.User
+		err = model.DB.Model(&model.User{}).Where(&model.User{
+			Base: model.Base{
+				ID: uint(uID),
+			},
+		}).Preload("Medium").First(&userModel).Error
+		if err != nil {
+			loggerx.Error(err)
+			errorx.Render(w, errorx.Parser(errorx.DBError()))
+			return
+		}
+		app.Users = append(app.Users, userModel)
 	}
 
 	err = model.DB.Model(&model.Space{}).Where(&model.Space{
