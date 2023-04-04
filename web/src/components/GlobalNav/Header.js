cf2f9cba0f095b6acfd9a917771b31f4ad065975
shmiitg <shubhamank002@gmail.com>
2023-04-04 17:06:56 +0530

    update navbar

--- web/src/components/GlobalNav/Header.js
+++ web/src/components/GlobalNav/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Layout, Divider, Button, Popover, List, Avatar } from 'antd';
-import { AppstoreOutlined } from '@ant-design/icons';
+import { Layout, Button, Popover, List, Avatar } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import SettingsIcon from '../Settings/img.svg';
 import AccountMenu from './AccountMenu';
 import OrganisationSelector from './OrganisationSelector';
 import { Link } from 'react-router-dom';
@@ -23,72 +24,75 @@ function Header() {
   return (
     <Layout.Header className="layout-header">
       <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-        <Divider type="vertical" />
-        {window.REACT_APP_ENABLE_MULTITENANCY === 'true' ? (
-          <div>
+        <div style={{ display: 'flex', height: '32px', gap: '14px' }}>
+          {window.REACT_APP_ENABLE_MULTITENANCY === 'true' ? (
+            <>
+              <Link style={{ display: 'flex' }} to="/organisation/create">
+                <Button icon={<PlusOutlined />} style={{ background: '#F1F1F1', border: 'none' }}>
+                  New Organisation
+                </Button>
+              </Link>
+              <OrganisationSelector />
+            </>
+          ) : organisationCount < 1 ? (
             <Link to="/organisation/create">
-              <Button>New Organisation</Button>
+              <Button icon={<PlusOutlined />} style={{ background: '#F1F1F1', border: 'none' }}>
+                New Organisation
+              </Button>
             </Link>
-            <OrganisationSelector />
-          </div>
-        ) : organisationCount < 1 ? (
-          <Link to="/organisation/create">
-            <Button>New Organisation</Button>
-          </Link>
-        ) : null}
-        {apps.length > 0 ? (
-          <>
-            <Divider type="vertical" />
-            <Popover
-              placement="bottom"
-              content={
-                <List
-                  grid={{
-                    gutter: 16,
-                    xs: 1,
-                    sm: 2,
-                    md: 4,
-                    lg: 4,
-                    xl: 6,
-                    xxl: 3,
-                  }}
-                  dataSource={apps}
-                  renderItem={(item) => (
-                    <List.Item>
-                      <a
-                        href={item.url}
-                        style={{ textDecoration: 'none', color: 'inherit' }}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {item.medium && item.medium.url ? (
-                          <img
-                            alt="logo"
-                            className="menu-logo"
-                            src={
-                              window.REACT_APP_ENABLE_IMGPROXY
-                                ? item.medium.url.proxy
-                                : item.medium.url?.raw
-                            }
-                          />
-                        ) : (
-                          <Avatar shape="square" size={35}>
-                            {item.name?.charAt(0)}
-                          </Avatar>
-                        )}
-                        <p>{item.name}</p>
-                      </a>
-                    </List.Item>
-                  )}
-                />
-              }
-              trigger="click"
-            >
-              <AppstoreOutlined />
-            </Popover>
-          </>
-        ) : null}
-        <Divider type="vertical" />
+          ) : null}
+          {apps.length > 0 ? (
+            <>
+              <Popover
+                placement="bottom"
+                content={
+                  <List
+                    grid={{
+                      gutter: 16,
+                      xs: 1,
+                      sm: 2,
+                      md: 4,
+                      lg: 4,
+                      xl: 6,
+                      xxl: 3,
+                    }}
+                    dataSource={apps}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <a
+                          href={item.url}
+                          style={{ textDecoration: 'none', color: 'inherit' }}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {item.medium && item.medium.url ? (
+                            <img
+                              alt="logo"
+                              className="menu-logo"
+                              src={
+                                window.REACT_APP_ENABLE_IMGPROXY
+                                  ? item.medium.url.proxy
+                                  : item.medium.url?.raw
+                              }
+                            />
+                          ) : (
+                            <Avatar shape="square" size={35}>
+                              {item.name?.charAt(0)}
+                            </Avatar>
+                          )}
+                          <p>{item.name}</p>
+                        </a>
+                      </List.Item>
+                    )}
+                  />
+                }
+                trigger="click"
+              >
+                <img src={SettingsIcon} alt="icon" />
+              </Popover>
+            </>
+          ) : null}
+        </div>
         <AccountMenu style={{ float: 'right' }} />
       </div>
     </Layout.Header>
