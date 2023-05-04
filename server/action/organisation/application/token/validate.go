851938340611e6eac748e039213491300c33aca3
elliot14A <akshithkatkuri14@gmail.com>
2023-05-04 12:17:29 +0530

    feat: updated status codes for token validation function

--- server/action/organisation/application/token/validate.go
+++ server/action/organisation/application/token/validate.go
@@ -12,10 +12,11 @@ import (
 	"github.com/factly/x/renderx"
 	"github.com/factly/x/validationx"
 	"github.com/go-chi/chi"
+	"gorm.io/gorm"
 )
 
-// ValidationBody request body
-type ValidationBody struct {
+// validationBody request body
+type validationBody struct {
 	Token string `json:"token" validate:"required"`
 }
 
@@ -42,13 +43,8 @@ func validate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	//parse applicaion_id
-	orgID, err := strconv.Atoi(r.Header.Get("X-Organisation"))
-	if err != nil {
-		errorx.Render(w, errorx.Parser(errorx.InvalidID()))
-		return
-	}
 
-	tokenBody := ValidationBody{}
+	tokenBody := validationBody{}
 	err = json.NewDecoder(r.Body).Decode(&tokenBody)
 	if err != nil {
 		loggerx.Error(err)
@@ -66,18 +62,18 @@ func validate(w http.ResponseWriter, r *http.Request) {
 	appToken := model.ApplicationToken{}
 	// Fetch all tokens for a application
 	err = model.DB.Model(&model.ApplicationToken{}).Preload("Application").Where(&model.ApplicationToken{
-		Token: tokenBody.Token,
+		Token: tokenBody.Token, ApplicationID: uint(id),
 	}).First(&appToken).Error
 
-	if err != nil || appToken.ApplicationID != uint(id) || appToken.Application.OrganisationID != uint(orgID) {
+	if err != nil {
 		loggerx.Error(err)
-		errorx.Render(w, errorx.Parser(errorx.RecordNotFound()))
+		if err == gorm.ErrRecordNotFound {
+			renderx.JSON(w, http.StatusUnauthorized, map[string]interface{}{"valid": false})
+			return
+		}
+		errorx.Render(w, errorx.Parser(errorx.InternalServerError()))
 		return
 	}
 
-	if tokenBody.Token == appToken.Token {
-		renderx.JSON(w, http.StatusOK, map[string]interface{}{"valid": true})
-	} else {
-		renderx.JSON(w, http.StatusUnauthorized, map[string]interface{}{"valid": false})
-	}
+	renderx.JSON(w, http.StatusOK, map[string]interface{}{"valid": true})
 }
