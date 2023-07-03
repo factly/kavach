818a9329bd80fc20bee80982e0c7726123a3e188
elliot14A <akshithkatkuri14@gmail.com>
2023-07-03 15:35:05 +0530

    feat: restructured organisation list endpoint

--- server/action/admin/organisation/list.go
+++ server/action/admin/organisation/list.go
@@ -1,7 +1,10 @@
 package organisation
 
 import (
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/factly/kavach-server/model"
 	"github.com/factly/x/errorx"
@@ -10,25 +13,48 @@ import (
 	"github.com/factly/x/renderx"
 )
 
+type response struct {
+	Nodes []model.Organisation `json:"nodes"`
+	Total int64                `json:"total"`
+}
+
 func list(w http.ResponseWriter, r *http.Request) {
 
-	orgIDs := r.URL.Query()["id"]
-
-	offset, limit := paginationx.Parse(r.URL.Query())
-
-	if len(orgIDs) > 0 {
-		offset = 0
-		limit = len(orgIDs)
+	orgIDsStr := r.URL.Query()["id"]
+	// if orgIDs is empty, then return all organisations
+	// else return organisations with given ids
+	res := &response{}
+	if len(orgIDsStr) == 0 {
+		err := model.DB.Model(&model.Organisation{}).Count(&res.Total).Find(&res.Nodes).Error
+		if err != nil {
+			loggerx.Error(err)
+			errorx.Render(w, errorx.Parser(errorx.DBError()))
+			return
+		}
+	} else {
+		orgIDList := strings.Split(orgIDsStr[0], ",")
+		var orgIDs []int
+		for _, idStr := range orgIDList {
+			id, err := strconv.Atoi(idStr)
+			if err != nil {
+				// Handle error if a string cannot be converted to an integer
+				loggerx.Error(err)
+				errorx.Render(w, errorx.Parser(errorx.GetMessage("Invalid OrgID", http.StatusBadRequest)))
+				return
+			}
+			orgIDs = append(orgIDs, id)
+		}
+		offset, limit := paginationx.Parse(r.URL.Query())
+		res.Total = int64(len(orgIDs))
+		err := model.DB.Model(&model.Organisation{}).Where("id IN (?)", orgIDs).Offset(offset).Limit(limit).Find(&res.Nodes).Error
+		if err != nil {
+			loggerx.Error(err)
+			errorx.Render(w, errorx.Parser(errorx.DBError()))
+			return
+		}
 	}
 
-	res := make([]model.Organisation, 0)
-
-	err := model.DB.Model(&model.Organisation{}).Where(orgIDs).Offset(offset).Limit(limit).Find(&res).Error
-	if err != nil {
-		loggerx.Error(err)
-		errorx.Render(w, errorx.Parser(errorx.DBError()))
-		return
-	}
-
+	log.Println("go to renderx.JSON")
 	renderx.JSON(w, http.StatusOK, res)
+	return
 }
