0193d74fbd1ea1b7bab1154d97a3dd96cbf59027
elliot14A <akshithkatkuri14@gmail.com>
2023-07-05 13:28:51 +0530

    feat: created endpoint to list the organisations of an application in admin

---
+++ server/action/admin/organisation/application/list_orgs.go
@@ -0,0 +1,45 @@
+package application
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/factly/kavach-server/model"
+	"github.com/factly/x/errorx"
+	"github.com/factly/x/loggerx"
+	"github.com/factly/x/renderx"
+	"github.com/go-chi/chi"
+)
+
+type response struct {
+	Nodes []model.Organisation `json:"nodes"`
+	Total int64                `json:"total"`
+}
+
+func ListOrgs(w http.ResponseWriter, r *http.Request) {
+	aId := chi.URLParam(r, "app_id")
+	appID, err := strconv.Atoi(aId)
+
+	if err != nil {
+		loggerx.Error(err)
+		errorx.Render(w, errorx.Parser(errorx.InvalidID()))
+		return
+	}
+	app := &model.Application{}
+	app.ID = uint(appID)
+	err = model.DB.Model(&model.Application{}).Preload("Organisations").Find(app).Error
+	log.Println("==============>", app.Name)
+	if err != nil {
+		loggerx.Error(err)
+		errorx.Render(w, errorx.Parser(errorx.DBError()))
+		return
+	}
+
+	res := &response{}
+	res.Nodes = app.Organisations
+	res.Total = int64(len(res.Nodes))
+
+	renderx.JSON(w, http.StatusOK, res)
+	return
+}
